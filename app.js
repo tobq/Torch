5b9763328ecb3939b7f0801828ac8cb218c7c047
Tobi <Tobi.akiny@gmail.com>
2016-08-26 22:56:02 +0100

    initial setup

--- app.js
+++ app.js
@@ -1,43 +1,86 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+var express = require('express'),
+    app = express(),
+    server = require("http").createServer(app),
+    io = require("socket.io").listen(server),
+    cors = require("cors")
+    PORT = process.env.PORT || 4000;
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+app.use(cors());
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
+server.listen(PORT,function(){
+    console.log("Running att http://localhost:"+PORT)
+});
+
+io.on('connection', function (socket) {
+    var A = Math.random() * 2 * Math.PI,
+        x = Math.random() * 100,
+        y = Math.random() * 100,
+        update = setInterval(function () {
+            x = Math.min(Math.max(2 * (Math.cos(A)) + x, 0), 100);
+            y = Math.min(Math.max(2 * (Math.sin(A)) + y, 0), 100);
+            io.emit("u", {id: socket.id, x: x, y: y});
+        }, 100);
+
+    console.log('> New connection');
+    socket.on('disconnect', function () {
+        clearInterval(update);
+        io.emit("d", socket.id);
     });
-  }
+    socket.on('a', function (a) {
+        A = a;
+    });
+
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
-});
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(function (req, res, next) {
+//     if (!req.cookies.sid) {
+//         req.cookies.sid = Math.random().toString().slice(2);
+//         res.cookie('sid', req.cookies.sid, {maxAge: 24 * 60 * 60 * 1000, httpOnly: true});
+//     }
+//     if (!sessions[req.cookies.sid]) {
+//         sessions[req.cookies.sid] = {
+//             angle: 0,
+//             x: Math.random() * 100,
+//             y: Math.random() * 100,
+//             timeout: setTimeout(function () {
+//                 delete sessions[req.cookies.sid];
+//             }, 1000 * 60)
+//         };
+//     } else {
+//         clearTimeout(sessions[req.cookies.sid].timeout);
+//         sessions[req.cookies.sid].timeout = setTimeout(function () {
+//             delete sessions[req.cookies.sid];
+//         }, 1000 * 60);
+//     }
+//     next();
+// });
+//
+// app.post("*", function (req, res) {
+//     res.send({
+//         x: sessions[req.cookies.sid].x,
+//         y: sessions[req.cookies.sid].y
+//     });
+//     sessions[req.cookies.sid].angle = req.body.angle;
+// });
+// app.get("*", function (req, res) {
+//     res.sendFile("index.html", {root: __dirname});
+// });
+//
+// app.listen(3000, function () {
+//     console.log('Listening at: http://localhost:3000');
+// });
+//
+// setInterval(function () {
+//     for (var session in sessions) {
+//         session = sessions[session];
+//         session.x = Math.min(Math.max(2 * (Math.cos(session.angle)) + session.x,0),100);
+//         session.y = Math.min(Math.max(2 * (Math.sin(session.angle)) + session.y,0),100);
+//     }
+// }, 100)
